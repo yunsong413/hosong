7cabe77a3aabc6801526d1c9018287cc8e9e96c1
unknown <songkiwi@gmail.com>
2022-12-03 18:11:05 +0900

    토요일 중간까지 작업

--- member_prc2.php
+++ member_prc2.php
@@ -9,12 +9,14 @@ $userid = $_GET['userid'];
 $name = $_GET['name'];
 $password = $_GET['password'];
 $memo = $_GET['memo'];
-$cmd = $_GET['cmd'] ? $_GET['cmd']:'insert';
+$cmd = $_GET['cmd'];
+//$cmd = $_GET['cmd'] ? $_GET['cmd']:'insert';
+echo "<a class='button' href='member_prc2.php'>리셋</a>";
+echo "<br>";
 
-if($cmd == 'update') {
-    $no = $_GET['no'];
-
+if($no!='' ) { // NO 값이 있을때
     $sql = "SELECT * from member where no=$no";
+
     $rows = mysqli_query($db,$sql);
     $row= mysqli_fetch_assoc($rows);
 
@@ -22,7 +24,28 @@ if($cmd == 'update') {
     $name = $row['name'];
     $password = $row['password'];
     $memo = $row['memo'];
+    $button_name = "수정";
+} else { // no 값이 있을때
+    $userid = $row[''];
+    $name = $row[''];
+    $password = $row[''];
+    $memo = $row[''];
+    $button_name = "추가";
+     
+}
 
+if($cmd == 'update') {
+    $no = $_GET['no'];
+
+    $sql = "SELECT * from member where no=$no";
+
+    $rows = mysqli_query($db,$sql);
+    $row= mysqli_fetch_assoc($rows);
+
+    $userid = $row['userid'];
+    $name = $row['name'];
+    $password = $row['password'];
+    $memo = $row['memo'];
 
 
 } else if($cmd == 'insert') {
@@ -51,32 +74,29 @@ if($cmd=="update"){
 }
     $sql="select * from member";
     $result = mysqli_query($db,$sql); 
-    while($rows= mysqli_fetch_assoc($result)) {
-        echo $rows['userid'];
-        echo $rows['name'];
-        echo $rows['password'];
-        echo $rows['memo'];
-        echo "<a class='button' href='member_prc.php?cmd=delete&no={$rows['no']}'>삭제</a>";
-        echo "<a class='button' href='member_prc.php?cmd=update&no={$rows['no']}'>수정</a>";
+    while($row= mysqli_fetch_assoc($result)) {
+        echo $row['no'];
+        echo"-";
+        echo $row['userid'];
+        echo"-";
+        echo $row['name'];
+        echo"-";
+        echo $row['password'];
+        echo"-";
+        echo $row['memo'];
+        echo "<a class='button' href='member_prc2.php?cmd=delete&no={$row['no']}'>삭제</a>";
+        echo "<a class='button' href='member_prc2.php?no={$row['no']}'>수정</a>";
         echo "<br>";
-    }
+    };
 ?>
 
-<form method="GET" action="member_prc.php">
+<form method="GET" action="member_prc2.php">
     <input type="hidden" name="cmd" value="<?=$cmd?>"/>
     <input type="hidden" name="no" value="<?=$no?>"/> 
-    <input type="text" name="userid" value="<?=$userid?>"/> 
-    <input type="text" name="name" value="<?=$name?>"/> 
-    <input type="text" name="password" value="<?=$password?>"/>
-    <input type="text" name="memo" value="<?=$memo?>"/>
-    <input type="submit"/>
-</form>
-<?
+    <input type="text" name="userid" value="<?=$userid?>" placeholder="userid"/> 
+    <input type="text" name="name" value="<?=$name?>" placeholder="name"/> 
+    <input type="text" name="password" value="<?=$password?>" placeholder="passoword"/>
+    <input type="text" name="memo" value="<?=$memo?>" placeholder="memo"/>
+    <input type="submit" value="<?=$button_name?>">
 
-echo $userid;
-echo $name;
-echo $password;
-echo $memo;
-echo "<a class='button' href='member_prc.php?cmd=delete&no={$rows['no']}'>삭제</a>";
-echo "<a class='button' href='member_prc.php?cmd=update&no={$rows['no']}'>수정</a>";
-?>  
+</form>
